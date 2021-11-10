e4f20f8eb9748524be260f3fc468c037a7d7e317
unknown <mallea_08@hotmail.com>
2021-11-10 15:00:18 -0300

    JS primera version

---
+++ index.js
@@ -0,0 +1,15 @@
+// declaramos la variable para mi nombre
+const firstname = "Franco"
+
+// declaramos la variable para mi apellido
+const lastname = "Mallea"
+
+// declaramos la variable para mi edad
+var age = 27
+
+// concatenamos las variables e imprimimos en la consola
+console.log(firstname + " " + lastname + " tiene " + age + " años")
+
+// le sumamos un numero a la variable age
+age = age+1
+console.log(firstname + " " + lastname + " tiene " + age + " años")
